7a31d84c4aed145d6e32fac83b869e42701c31a2
Douglas Silva <dssilva182@gmail.com>
2024-06-11 23:10:42 -0300

    dado sequencia no projeto, ainda construindo os scripts em python

--- Simple exploratory analysis/analysis.py
+++ Simple exploratory analysis/analysis.py
@@ -1,5 +1,7 @@
 # First, let's import all the required libraries
 import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import f_oneway
 
 # Now, let's import our database. It is in a .sav format, so we need to 
 dados = pd.read_spss('dados_lista1.sav')
@@ -36,3 +38,40 @@ frequency_origin_region = pd.crosstab(dados['origem'], 'count')
 frequency_origin_region['percentage'] = frequency_origin_region/len(dados)*100
 
 frequency_origin_region
+
+# Question 2 - graph these tables
+# Let's do it using subplots:
+    
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(9, 6), sharey=True)
+ax1.bar(frequency_table.index, frequency_table['percentage'])
+ax1.set_ylabel('Proportion of total sample')
+ax2.bar(frequency_origin_region.index, frequency_origin_region['percentage'])
+ax3.bar(frequency_marital.index, frequency_marital['percentage'])
+plt.show()
+
+# Question 3 - Compare, statistically (α=5%), whether there are differences in the distribution of the number of children and the average age depending on the regions of origin of the workers.
+
+# First, let's have a look in the distribution of our dependent variables (age and number of childs), using histograms.
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(9, 4))
+ax1.hist(dados['idade'])
+ax1.set_ylabel('Idade (anos)')
+ax2.hist(dados['n_mero_d'])
+ax2.set_ylabel('Número de filhos')
+plt.show()
+
+# We can note an assymetric distribution for both variables, but let's compare these variables by using analysis of variance
+anova_age = f_oneway(dados['idade'][dados['origem'] == 'Interior'],
+                     dados['idade'][dados['origem'] == 'Capital'],
+                     dados['idade'][dados['origem'] == 'Outro'])
+
+anova_childs = f_oneway(dados['n_mero_d'][dados['origem'] == 'Interior'],
+                     dados['n_mero_d'][dados['origem'] == 'Capital'],
+                     dados['n_mero_d'][dados['origem'] == 'Outro'])
+
+# For both tests it was not possible to see any effect of origin. 
+
+
+# Question 4 - Calculate the average salary and average age of single and married people
+age_marital = dados.groupby('estado_c')['idade'].describe()
+salary_marital = dados.groupby('estado_c')['sal_rio1'].describe()
+
